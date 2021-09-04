bd3ba0aff4d3df8d5be244411a5506210a2cf7d7
Ankit Kumar <ankit628792@gmail.com>
2021-09-04 14:48:27 +0530

    added_subscribe_text

--- pages/subscribe.js
+++ pages/subscribe.js
@@ -6,6 +6,7 @@ function subscribe() {
     const router = useRouter();
     const [isSending, setIsSending] = useState(false)
     const [mail, setMail] = useState()
+    const [subscribed, setSubscribed] = useState(false);
     
     const subscribe = async () => {
         setIsSending(true);
@@ -19,10 +20,9 @@ function subscribe() {
       
           const response = await res.json();
           if(response){
-              router.push('/');
+              setSubscribed(true);
           }
           setIsSending(false)
-        
     }
     return (
         <>
@@ -35,10 +35,11 @@ function subscribe() {
                         <div className="p-5 sm:px-10 max-w-3xl text-center text-gray-700 mx-auto">
                             <h1 className="text-4xl md:text-5xl mb-3 font-semibold">Subscribe to Our Newsletter</h1>
                             <p className="text-lg md:text-xl">Join TheNeuron.club to bet directly on the outcome of events. We've built a next gen betting platform for you to bet on your opinion.</p>
-                            <div className="flex max-w-md mt-8 rounded-lg gradient-shadow mx-auto">
+                            <div className="flex max-w-md my-8 rounded-lg gradient-shadow mx-auto">
                                 <input type="text" className="flex-grow max-w-xs p-3 px-4 focus:outline-none focus:border focus:border-blue-500 placeholder-gray-400 font-normal" value={mail} required placeholder="Type Your Email Address ..." onChange={(e) => setMail(e.target.value)} />
                                 <button className="px-6 py-3 text-lg text-white font-semibold gradient-bg" onClick={subscribe}>{isSending ? `Subscribing..` : `Subscribe`}</button>
                             </div>
+                           {subscribed && <h1 className="text-4xl md:text-5xl mt-4 font-semibold">Thanks for Subscribing</h1>}
                         </div>
             </div>
         </>
