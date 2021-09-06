853bff9b448e1972507ecf8d899d9bf59a473c91
Ankit Kumar <ankit628792@gmail.com>
2021-09-06 10:37:53 +0530

    ui4

--- components/Question.jsx
+++ components/Question.jsx
@@ -7,15 +7,17 @@ function Question({ question }) {
                 <h1 className="line-clamp-4 text-base mt-1 sm:mt-0">{question.question}</h1>
                 <img src={question.image} layout="fill" className="w-10 h-10 border-4 border-blue-100 gradient-shadow-lg hover:scale-105 transition-md object-cover rounded-full -translate-x-3 sm:-translate-x-0" />
             </div>
-            <h1 className="text-center flex my-2"><ClockIcon className="h-7 text-blue-500" /> <span className="inline-block w-14">{Math.floor(Math.random() * 10)}h left</span></h1>
+            <div className="h-4 w-full"></div>
+            {/* <h1 className="text-center flex my-2"><ClockIcon className="h-7 text-blue-500" /> <span className="inline-block w-14">{Math.floor(Math.random() * 10)}h left</span></h1> */}
             <div className="flex items-center justify-between mt-auto">
                 <div className="text-sm">
                     <h1 className="text-gray-400">Volume</h1>
                     <h2>{question.volume}</h2>
                 </div>
-                <div className="text-right text-sm">
-                    <h1 className="text-gray-400">Bets</h1>
-                    <h1>{Math.floor(Math.random() * 100)}/100</h1>
+                <div className="text-right flex flex-col items-end text-sm">
+                    {/* <h1 className="text-gray-400">Bets</h1>
+                    <h1>{Math.floor(Math.random() * 100)}/100</h1> */}
+                    <ClockIcon className="h-6 sm:h-7 text-blue-500" /> <span className="">{Math.floor(Math.random() * 10)}h left</span>
                 </div>
             </div>
         </div>
