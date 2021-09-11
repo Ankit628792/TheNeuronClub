5f0c4f5a9bb0c79c2533686cee7cd818adbfd8e3
Ankit <ankit628792@gmail.com>
2021-09-11 18:07:04 +0530

    Update early_signup.js

--- pages/api/early_signup.js
+++ pages/api/early_signup.js
@@ -44,9 +44,40 @@ const early_signup = async (req, res) => {
         try {
             const earlySignup = new EarlySignup({ email:req.body});
             const emailRegistered = await earlySignup.save();
-            if(emailRegistered){
-                sendMail(emailRegistered)
+            try {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.zoho.in',
+            secure: true,
+            port: 465,
+            auth: {
+                user: `dev@theneuron.club`,
+                pass: `Club1234567890`,
+            },
+        })
+        const mailData = {
+            from: `dev@theneuron.club`,
+            to: `ankit628792@gmail.com`,
+            subject: `New Signup`,
+            text:   " Sent from: " + emailRegistered.email,
+            html: `<div>${emailRegistered.email} recently Sign Up The Neuron club</div>`
+        }
+
+        transporter.sendMail(mailData, function (err, info) {
+            if (err){
+                console.log(err)
+                res.status(301).send({ms: 'error', ss: emailRegistered})
             }
+            else
+            {
+                res.status(302).send({ms: 'sent', ss: emailRegistered})
+                console.log(info)
+            }
+        })
+       
+    }
+    catch (error) {
+        console.log(error)
+    }
             res.status(201).send(req.body)
         }
         catch (error) {
