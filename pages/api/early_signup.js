79e0eb0dc499952f9f586424b214caf543333288
Ankit <ankit628792@gmail.com>
2021-09-11 18:16:51 +0530

    Update early_signup.js

--- pages/api/early_signup.js
+++ pages/api/early_signup.js
@@ -2,35 +2,31 @@ import connectDB from '../../server/db/mongodb';
 import EarlySignup from '../../server/db/models/earlySignup';
 const nodemailer = require('nodemailer')
 
-const sendMail = (emailRegistered) => {
+const sendMail = (email) => {
+    console.log(email)
     try {
         const transporter = nodemailer.createTransport({
             host: 'smtp.zoho.in',
             secure: true,
             port: 465,
             auth: {
-                user: `dev@theneuron.club`,
-                pass: `Club1234567890`,
+                user: process.env.mail_user,
+                pass: process.env.mail_pass,
             },
         })
         const mailData = {
-            from: `dev@theneuron.club`,
-            to: `ankit628792@gmail.com`,
+            from: process.env.mail_user,
+            to: process.env.mail_to,
             subject: `New Signup`,
-            text:   " Sent from: " + emailRegistered,
-            html: `<div>${emailRegistered.email} recently Sign Up The Neuron club</div>`
+            text:   " Sent from: " + email,
+            html: `<div>${email} recently Sign Up The Neuron club</div>`
         }
 
         transporter.sendMail(mailData, function (err, info) {
-            if (err){
+            if (err)
                 console.log(err)
-                res.status(301).send({ms: 'error'})
-            }
             else
-            {
-                res.status(302).send({ms: 'sent'})
                 console.log(info)
-            }
         })
        
     }
@@ -44,40 +40,9 @@ const early_signup = async (req, res) => {
         try {
             const earlySignup = new EarlySignup({ email:req.body});
             const emailRegistered = await earlySignup.save();
-            try {
-        const transporter = nodemailer.createTransport({
-            host: 'smtp.zoho.in',
-            secure: true,
-            port: 465,
-            auth: {
-                user: `dev@theneuron.club`,
-                pass: `Club1234567890`,
-            },
-        })
-        const mailData = {
-            from: `dev@theneuron.club`,
-            to: `ankit628792@gmail.com`,
-            subject: `New Signup`,
-            text:   " Sent from: " + emailRegistered.email,
-            html: `<div>${emailRegistered.email} recently Sign Up The Neuron club</div>`
-        }
-
-        transporter.sendMail(mailData, function (err, info) {
-            if (err){
-                console.log(err)
-                res.status(301).send({ms: 'error'})
+            if(emailRegistered){
+                sendMail(emailRegistered.email)
             }
-            else
-            {
-                res.status(302).send({ms: 'sent'})
-                console.log(info)
-            }
-        })
-       
-    }
-    catch (error) {
-        console.log(error)
-    }
             res.status(201).send(emailRegistered)
         }
         catch (error) {
