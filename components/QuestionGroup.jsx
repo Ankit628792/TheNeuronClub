1ffe9b0778e5f736b57004c554db151e8241be06
Ankit Kumar <ankit628792@gmail.com>
2021-09-15 17:54:21 +0530

    q page

--- components/QuestionGroup.jsx
+++ components/QuestionGroup.jsx
@@ -8,12 +8,13 @@ function QuestionGroup({ questions, category }) {
             <div className="p-5 py-10 sm:p-10 xl:px-20">
                 <div className="flex justify-between border-b-2 mb-4 pb-2 border-gray-200">
                     <h1 className="text-2xl sm:text-3xl font-semibold  text-gray-700">{category}</h1>
-                    <Link href='/question/questions'>
+                    <Link href='/question/'>
                         <h1 className="flex items-center text-base sm:text-lg cursor-pointer text-blue-500 sm:pr-4">View All <ArrowRightIcon className="h-7 mx-2" /></h1>
                     </Link>
                 </div>
                 <div className="question__group grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                     {
+                        questions && questions?.length > 0 &&
                         questions.map((item, i) => (
                             <Question question={item} key={i} />
                         ))
