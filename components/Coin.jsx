0b2a8b268249ddd09e88a19f0aa2da30d2c85d21
Ankit Kumar <ankit628792@gmail.com>
2021-09-25 19:52:08 +0530

    coin img

--- components/Coin.jsx
+++ components/Coin.jsx
@@ -1,12 +1,11 @@
 import Image from "next/image"
 
-function Coin({width, height}) {
+function Coin({ width, height }) {
     return (
         <>
-        <div className={`relative inline-block mr-1 w-${width} h-${height}`}>
-
-            <Image src="/images/coin.jfif" layout="fill" className="rounded-full w-4 h-4" />
-        </div>
+            <div className={`relative inline-block mr-1 w-${width} h-${height}`}>
+                <Image src="/images/coin.jpg" layout="fill" className="rounded-full w-4 h-4" />
+            </div>
         </>
     )
 }
