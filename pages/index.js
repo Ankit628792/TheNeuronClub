52e820a21c1e4520216f8a24e003e6bab39ca314
Ankit Kumar <ankit628792@gmail.com>
2021-09-28 16:09:47 +0530

    server 5

--- pages/index.js
+++ pages/index.js
@@ -42,11 +42,11 @@ export default function Home({ questions }) {
 
 export async function getStaticProps() {
   // const questions = await fetch('https://sample-api-data.vercel.app/api/tnc/questions').then((res) => res.json());
-  const questions = await fetch(`${process.env.HOST}/api/question/get_questions`).then((res) => res.json());
-  return {
-    props: {
-      questions
-    }
-  }
+  // const questions = await fetch(`${process.env.HOST}/api/question/get_questions`).then((res) => res.json());
+  // return {
+  //   props: {
+  //     questions
+  //   }
+  // }
 }
 
