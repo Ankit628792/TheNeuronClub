5128bd31951101cd611b3f4da0b55be955065b76
Ankit Kumar <ankit628792@gmail.com>
2021-10-06 21:58:57 +0530

    content

--- components/Steps.jsx
+++ components/Steps.jsx
@@ -8,8 +8,13 @@ function Steps({ step, type }) {
             <div className="p-5 sm:px-10 lg:px-20">
                 <h1 className="text-blue-400 text-2xl font-medium">Step {step?.no}</h1>
                 <h1 className="text-3xl lg:text-5xl font-bold my-2 md:my-3 text-gray-800">{step.heading}</h1>
-                <p className="text-lg mb-2">{step?.desc}</p>
-                {step?.other && <ul>
+                <p className="text-lg">{step?.desc}</p>
+                {step?.sub_desc && <ul>
+                    {step?.sub_desc?.map((item, i) => (
+                        <li key={i}><p><b>Tip: </b>{item}</p></li>
+                    ))}
+                    </ul>}
+                {step?.other && <ul className="mt-2">
                     {
                         step?.other?.map((item, i) => (
                             <li key={i}><p>{i + 1}. {item}</p></li>
