23f74c833d3bcf477ede4e3edebb3bbbc9c946c2
Ankit Kumar <ankit628792@gmail.com>
2021-10-08 13:27:49 +0530

    new deploy 5

--- pages/question/index.js
+++ pages/question/index.js
@@ -139,7 +139,7 @@ function index({ data }) {
 export default index
 
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps() {
     const data = await fetch(`${process.env.HOST}/api/question/get_questions`).then(res => res.json());
     return {
         props: {
