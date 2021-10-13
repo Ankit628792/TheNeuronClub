e3e5ca835bf467c18a830a8253f1997573fde84b
Ankit Kumar <ankit628792@gmail.com>
2021-10-13 14:15:06 +0530

    goLive Que

--- server/controllers/question.js
+++ server/controllers/question.js
@@ -9,8 +9,10 @@ const createQuestion = async (req, res) => {
         res.status(400).send('Error');
     }
     else {
-        const link = `${host}/question/${saveQuestion?._id}`;
+        const link = `${process.env.host}/question/${saveQuestion?._id}`;
+        console.log(link)
         const data = { subject: `New Question added`, text: link, email: `ankit628792@gmail.com`, html: `Click <a href="${link}" target="_blank">View Question</a>` };
+        console.log(data)
         const result = await sendEMail(data);
         console.log(result)
         res.status(201).send(saveQuestion)
