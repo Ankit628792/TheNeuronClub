6242dc5e4e4f62b0ebef3d5e06b0f6d861dd76e0
Ankit Kumar <ankit628792@gmail.com>
2021-11-17 09:24:29 +0530

    final changes

--- app/store.js
+++ app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filter from "../slices/filter";
 import loader from "../slices/loader";
-import userAuth from "../slices/userAuth";
 import userBalance from "../slices/userBalance";
 
 export const store = configureStore({
   reducer: {
-    session: userAuth,
     balance: userBalance,
     filter: filter,
     loader: loader
