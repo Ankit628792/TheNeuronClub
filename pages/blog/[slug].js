a3fe44b8cd50ee78cf64ca547b032bdafb76d135
Ankit <ankit628792@gmail.com>
2022-02-15 21:59:19 +0530

    'blog'

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,38 +1,43 @@
 import { sanityClient, urlFor } from "../../sanity";
 import PortableText from 'react-portable-text'
+import Head from "next/head";
 
 
 const Post = ({ post }) => {
-    return <main className="text-white py-10">
-        <img className="h-96 object-contain w-full" src={urlFor(post.mainImage)?.url()} alt="" />
-        <article className="max-w-3xl mx-auto p-5">
-            <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
-            <p className="text-xl font-light text-gray-100 mb-2">{post.description}</p>
+    return (
+        <>
+            <Head><title>Blog | {post.title}</title></Head>
+            <main className="text-white py-10">
+                <img className="h-96 object-contain w-full" src={urlFor(post.mainImage)?.url()} alt="" />
+                <article className="max-w-3xl mx-auto p-5">
+                    <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+                    <p className="text-xl font-light text-gray-100 mb-2">{post.description}</p>
 
-            <div className="flex items-center space-x-2">
-                <img className="h-10 w-10 rounded-full" src={urlFor(post.author.image)?.url()} alt="" />
-                <p className="text-base font-extralight">Blog post by <span className="text-yellow-300">{post.author.name}</span> - Published at {new Date(post._createdAt).toDateString()}, {new Date(post._createdAt).toLocaleTimeString()}</p>
-            </div>
+                    <div className="flex items-center space-x-2">
+                        <img className="h-10 w-10 rounded-full" src={urlFor(post.author.image)?.url()} alt="" />
+                        <p className="text-base font-extralight">Blog post by <span className="text-yellow-300">{post.author.name}</span> - Published at {new Date(post._createdAt).toDateString()}, {new Date(post._createdAt).toLocaleTimeString()}</p>
+                    </div>
 
-            <div className="mt-10">
-                <PortableText
-                    dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
-                    projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
-                    content={post.body}
-                    serializers={{
-                        h1: (props) => <h1 className="text-3xl font-bold my-5" {...props} />,
-                        h2: (props) => <h1 className="text-2xl font-bold my-5" {...props} />,
-                        h3: (props) => <h1 className="text-xl font-bold my-5" {...props} />,
-                        p: (props) => <p className="text-lg font-medium my-5" {...props} />,
-                        li: ({ children }) => <li className="ml-4 list-disc"> {children} </li>,
-                        link: ({ href, children }) => <a href={href} className="text-yellow-300 hover:underline"> {children} </a>,
-                    }
-                    }
-                />
-            </div>
-        </article>
-
-    </main>;
+                    <div className="mt-10">
+                        <PortableText
+                            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+                            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+                            content={post.body}
+                            serializers={{
+                                h1: (props) => <h1 className="text-3xl font-bold my-5" {...props} />,
+                                h2: (props) => <h1 className="text-2xl font-bold my-5" {...props} />,
+                                h3: (props) => <h1 className="text-xl font-bold my-5" {...props} />,
+                                p: (props) => <p className="text-lg font-medium my-5" {...props} />,
+                                li: ({ children }) => <li className="ml-4 list-disc"> {children} </li>,
+                                link: ({ href, children }) => <a href={href} className="text-yellow-300 hover:underline"> {children} </a>,
+                            }
+                            }
+                        />
+                    </div>
+                </article>
+            </main>
+        </>
+    );
 };
 
 export default Post;
